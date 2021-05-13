93e0370740c6e8475ad1ea1bf67f198229ce310f
Dorothy2020 <wangarraakoth@gmail.com>
2021-05-13 17:45:29 +0300

    quiz

---
+++ index.js
@@ -0,0 +1,97 @@
+ for ( var i = 1; i <= 100; i++ )
+ {
+   if ( i%3 === 0 && i%5 === 0 )
+   {
+    console.log( i + " FizzBuzz" );
+  }
+   else if ( i%3 === 0 ) 
+  {
+     console.log(i+ " Fizz" );
+  }
+  else if ( i%5 === 0 ) 
+   {
+     console.log(i+ " Buzz" );
+  }
+ }
+
+ let total = 0;
+ for (let x = 0; x < 1000; x++)
+ {
+     if (x % 3 === 0 || x % 5 === 0)     {
+       total += x;
+    }
+ }
+console.log(total);
+
+
+
+ b=-2;
+ c=-6;
+ d= 0;
+ f=-1;
+ a=5;
+ if (a>b && a>c && a>d && a>f)
+ {
+     console.log(a);
+ }
+else if (b>a && b>c && b>d && b>f)
+ {
+     console.log(b);
+ if (c>a && c>b && c>d && c>f)
+ {
+     console.log(c);
+ }
+ else if (d>a && d>c && d>b && d>f)
+ {
+     console.log(d);
+ }
+ else
+{
+     console.log(f);
+}
+
+
+
+// let students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
+
+//             let Avgmarks = 0;
+
+//   for (let i=0; i < students.length; i++) {
+//           Avgmarks += students[i][1];
+//           }
+
+//       if (avg < 60){
+//           console.log("Grade : F");      
+//           } 
+//        else if (avg < 70) {
+//                 console.log("Grade : D"); 
+//                   } 
+//       else if (avg < 80) 
+//              {
+//                 console.log("Grade : C"); 
+//       } else if (avg < 90) {
+//                 console.log("Grade : B"); 
+//        } else if (avg < 100) {
+//                 console.log("Grade : A"); 
+// }
+
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
